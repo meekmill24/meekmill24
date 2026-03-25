f6cb9eea049e07ebe3b12f2525055dc312f80151
meekmill24 <amoafop08@gmail.com>
2026-03-26 04:39:12 +0700

    Calibrate Live Chat protocol to hide redundant Tawk.to trigger and use premium Draggable interface exclusively

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,6 +39,9 @@ export default function RootLayout({
                 <Script id="tawk-to" strategy="lazyOnload">
                   {`
                     var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+                    Tawk_API.onLoad = function() {
+                      Tawk_API.hideWidget();
+                    };
                     (function(){
                     var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
                     s1.async=true;
